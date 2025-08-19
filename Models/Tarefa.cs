3100754f5c8ef5e037cfbc9f81e873e55c680bb9
Marcos Alfredo Saffran <marcossaffran@gmail.com>
2025-08-19 09:46:22 -0300

    Refactor: Implement task validation and CRUD operations in TarefaController; add migration for Tarefas table

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TrilhaApiDesafio.ModelViews;
 
 namespace TrilhaApiDesafio.Models
 {
@@ -12,5 +13,19 @@ namespace TrilhaApiDesafio.Models
         public string Descricao { get; set; }
         public DateTime Data { get; set; }
         public EnumStatusTarefa Status { get; set; }
+
+        public ErrosDeValidacao ValidarTarefa()
+        {
+            var errosDeValidacao = new ErrosDeValidacao();
+
+            if (Data == DateTime.MinValue)
+                errosDeValidacao.Mensagens.Add("A data da tarefa não pode ser vazia");
+
+            if (!Enum.IsDefined(typeof(EnumStatusTarefa), Status))
+                errosDeValidacao.Mensagens.Add("O status da tarefa não pode ser inválido");
+
+
+            return errosDeValidacao;
+        }
     }
 }
